e15eec06f818828da9d9b8d78dc774a47700689b
jitendra giriya <jitendragiriya@gmail.com>
2023-12-16 20:35:25 +0530

    new changes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { GameProvider } from "@/context/gameContext";
 import TicHome from "@/pages/home";
 
 export default function Home() {
   return (
-    <GameProvider>
       <main className="flex flex-col items-center justify-center h-screen duration-100 bg-gradient-to-r from-indigo-500 to-pink-500 hue-rotate-[360deg]">
         <TicHome />
-      </main>
-    </GameProvider>
+      </main> 
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
     //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
